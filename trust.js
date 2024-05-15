b9dac66c9204ed748cd9a31a26db1fda418b3889
trustchakombera <trustchakombera00@gmail.com>
2024-05-15 10:59:07 +0200

    updated

---
+++ trust.js
@@ -0,0 +1,9 @@
+var sidemenu = document.getElementsByClassName("sidemenu");
+
+    function  openmenu() {
+        sidemenu.style.right ="0"
+    };
+
+    function  closemenu() {
+        sidemenu.style.right ="-200px";
+    }
